0c99e82cea3055f33a9f269cc71d9f9c4dffed8b
Jake <2bjake@users.noreply.github.com>
2021-12-01 09:01:25 -0600

    day1

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -1,9 +1,21 @@
 import Algorithms
 
-public func partOne() {
+public func countDepthIncreases(in depths: [Int]) -> Int {
+  depths
+    .adjacentPairs()
+    .reduce(0) { count, pair in
+      pair.0 < pair.1 ? count + 1 : count
+    }
+}
 
+public func partOne() {
+  print(countDepthIncreases(in: input)) // 1655
 }
 
 public func partTwo() {
+  let depthSums = input
+    .windows(ofCount: 3)
+    .map { $0.reduce(0, +) }
 
+  print(countDepthIncreases(in: depthSums)) // 1683
 }
